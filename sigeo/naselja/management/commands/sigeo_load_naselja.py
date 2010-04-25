04788a06f6d0d66dcd3ee44a93fa85f2dc22cfa7
Gasper Zejn <zejn@kiberpipa.org>
2010-04-25 23:20:59 +0200

    Popravi naselja, da uporabljajo ForeignKey.

--- sigeo/naselja/management/commands/sigeo_load_naselja.py
+++ sigeo/naselja/management/commands/sigeo_load_naselja.py
@@ -1,12 +1,13 @@
 import re
 from django.core.management.base import BaseCommand
 
-naselje_re = re.compile('(?P<ob_id>\d{3})(?P<ob_ime>.{30})(?P<ob_uime>.{50})(?P<ob_dj>.{50})(?P<na_mid>\d{8})(?P<na_id>\d{3})(?P<na_ime>.{30})(?P<na_uime>.{50})(?P<na_dj>.{50})(?P<d_od>\d\d\.\d\d\.\d{4} \d\d:\d\d:\d\d)(?P<povrsina>[\d.]{15})')
+naselje_re = re.compile('(?P<ob_id>\d{3}).{30}.{50}.{50}(?P<na_mid>\d{8})(?P<na_id>\d{3})(?P<na_ime>.{30})(?P<na_uime>.{50})(?P<na_dj>.{50})(?P<d_od>\d\d\.\d\d\.\d{4} \d\d:\d\d:\d\d)(?P<povrsina>[\d.]{15})')
 
 class Command(BaseCommand):
 	help = "Nalozi bazo naselij iz tekstovne datoteke"
 	
 	def handle(self, *args, **options):
+		from sigeo.obcine.models import Obcina
 		from sigeo.naselja.models import Naselje
 		from datetime import date
 		import codecs
@@ -25,6 +26,7 @@ class Command(BaseCommand):
 			d_od = [int(i) for i in re.match('^(\d\d)\.(\d\d)\.(\d{4})' , data['d_od']).groups()]
 			d_od.reverse()
 			data['d_od'] = date(*d_od)
-			
+			data['obcina'] = Obcina.objects.get(ob_id=int(data['ob_id']))
+			del data['ob_id']
 			n = Naselje(**data)
 			n.save()
--- sigeo/naselja/models.py
+++ sigeo/naselja/models.py
@@ -1,5 +1,6 @@
 # *-* coding: utf-8 *-*
 from django.db import models
+from sigeo.obcine.models import Obcina
 
 class Naselje(models.Model):
 	na_mid = models.IntegerField(help_text="identifikator naselja", primary_key=True)
@@ -7,12 +8,9 @@ class Naselje(models.Model):
 	na_ime = models.CharField(help_text="ime naselja", max_length=30)
 	na_uime = models.CharField(help_text="ime naselja uradno", max_length=50)
 	na_dj = models.CharField(help_text="ime naselja dvojezično", max_length=50)
-	ob_id = models.IntegerField(help_text="šifra občine")
-	ob_ime = models.CharField(help_text="ime občine", max_length=30)
-	ob_uime = models.CharField(help_text="uradno ime občine", max_length=50)
-	ob_dj = models.CharField(help_text="dvojezično ime občine", max_length=50)
 	d_od = models.DateField(help_text="datum veljavnosti")
 	povrsina = models.DecimalField(max_digits=15, decimal_places=4)
+	obcina = models.ForeignKey(Obcina)
 	
 	def __unicode__(self):
 		return self.na_uime
