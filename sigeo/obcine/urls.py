62856dadce5c638a1340bf7e5e7c31887f270c94
Gasper Zejn <zejn@kiberpipa.org>
2010-09-29 17:44:36 +0200

    Update obcina, fix multipolygon obcine.

--- sigeo/obcine/urls.py
+++ sigeo/obcine/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('sigeo.obcine.views',
     url(r'^$', 'index', name='obcina_index'),
     url(r'^polys/(?P<id>\d+).js$', 'polys', name='obcina_polys'),
+    url(r'^reverse/$', 'reverse_obcina', name='reverse_obcina'),
 )
 
--- sigeo/obcine/views.py
+++ sigeo/obcine/views.py
@@ -2,17 +2,32 @@ from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.views.decorators.cache import cache_page
+import simplejson
 # Import custom modules
 from sigeo.obcine.models import Obcina
 
 #@cache_page(60*15)
 def polys(request, id):
     obc = Obcina.objects.get(ob_id=int(id))
-    poly = ['new GLatLng(%s, %s)' % (y, x) for x,y in obc.the_geom.tuple[0][0]]
-    text = 'polyById[%s] = new GPolygon([%s], "#f33f00", 5, 1, "#ff0000", 0.2);' % (obc.ob_id, ','.join(poly))
+
+    polytext = []
+    for polytuple in obc.the_geom.tuple:
+        poly = ['new google.maps.LatLng(%s, %s)' % (y, x) for x,y in polytuple[0]]
+        polytext.append('new google.maps.Polygon({map: map, fillColor: "#f33f00", fillOpacity: 0.2, paths: [%s], strokeWeight: 5, strokeOpacity: 1, strokeColor: "#ff0000"})' % (','.join(poly),))
+    
+    text = 'polyById[%s] = [%s];' % (obc.ob_id, ','.join(polytext))
     all_poly = [text]
-    all_poly.append('''polygon = polyById[%s]; map.addOverlay(polygon);''' % obc.ob_id)
+    all_poly.append('''polygons = polyById[%s];''' % obc.ob_id)
     return HttpResponse('\n'.join(all_poly), mimetype='text/javascript')
+
+def reverse_obcina(request):
+    latlon = request.GET.get('ll', None)
+    obcina_id = None
+    if latlon:
+        lat, lon = [float(i) for i in latlon.split(',')]
+        obcina = Obcina.objects.get(the_geom__contains='POINT(%f %f)' % (lon, lat))
+        obcina_id = obcina.ob_id
+    return HttpResponse(simplejson.dumps({'status':'ok', 'obcina_id': obcina_id}))
 
 def index(request):
     return render_to_response('obcina/index.html', {'obcine': Obcina.objects.all()})
